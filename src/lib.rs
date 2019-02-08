f37b946a4ee9bc0975f10a1bda07589f199255dc
Shady Khalifa <shekohex@gmail.com>
2019-02-08 14:17:55 +0200

    last updates

--- src/lib.rs
+++ src/lib.rs
@@ -14,36 +14,52 @@ cfg_if! {
     }
 }
 
+/// Generate a 32-byte curve25519 key, given a 32-byte curve25519 secret key
+/// and a 32-byte curve22519 public key.
+///
+/// If the public argument is the predefined basepoint value (9 followed by all
+/// zeros), then this function will calculate a curve25519 public key.
 #[wasm_bindgen]
 pub fn curve25519(secret: &[u8], public: &[u8]) -> Vec<u8> {
     let sk: ArrayVec<[u8; 32]> = secret.iter().cloned().take(32).collect();
-    let sk = sk
-        .into_inner()
-        .unwrap_or_else(|_| panic!("secret key must be a 32 bit array lenght."));
+    let sk = sk.into_inner().unwrap_or_else(|_| {
+        panic!("secret key must be a 32 bit array lenght.")
+    });
     let pk: ArrayVec<[u8; 32]> = public.iter().cloned().take(32).collect();
-    let pk = pk
-        .into_inner()
-        .unwrap_or_else(|_| panic!("public key must be a 32 bit array lenght."));
+    let pk = pk.into_inner().unwrap_or_else(|_| {
+        panic!("public key must be a 32 bit array lenght.")
+    });
     curve25519::curve25519(sk, pk).to_vec()
 }
 
+/// Generate a 32-byte curve25519 secret key.
+/// from a random 32-byte value, that is used as the base
 #[wasm_bindgen]
 pub fn curve25519_sk(rand: &[u8]) -> Vec<u8> {
     let random_bytes: ArrayVec<[u8; 32]> =
         rand.iter().cloned().take(32).collect();
-    let random_bytes = random_bytes
-        .into_inner()
-        .unwrap_or_else(|_| panic!("random bytes must be a 32 bit array lenght."));
-    curve25519::curve25519_sk(Some(random_bytes))
-        .unwrap()
-        .to_vec() // unwrap here is safe, since we sure that we give it a random bytes
+    let random_bytes = random_bytes.into_inner().unwrap_or_else(|_| {
+        panic!("random bytes must be a 32 bit array lenght.")
+    });
+
+    if let Ok(sk) = curve25519::curve25519_sk(Some(random_bytes)) {
+        return sk.to_vec();
+    } else {
+        // ok here i'm abort, which is the behavior of `unwrap()`
+        // but without a lot of generated code
+        std::process::abort()
+    }
 }
 
+/// Generate a 32-byte curve25519 public key.
+///
+/// Calls curve25519 with the public key set to the basepoint value of 9
+/// followed by all zeros.
 #[wasm_bindgen]
 pub fn curve25519_pk(secret_key: &[u8]) -> Vec<u8> {
     let sk: ArrayVec<[u8; 32]> = secret_key.iter().cloned().take(32).collect();
-    let sk = sk
-        .into_inner()
-        .unwrap_or_else(|_| panic!("secret key must be a 32 bit array lenght."));
+    let sk = sk.into_inner().unwrap_or_else(|_| {
+        panic!("secret key must be a 32 bit array lenght.")
+    });
     curve25519::curve25519_pk(sk).to_vec()
 }
